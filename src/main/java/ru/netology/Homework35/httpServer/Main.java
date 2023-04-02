eb7e4168cc452b1d5de8e9a1d6a8a0f9e26b3142
PowerOleg <geraskin.oleg@mail.ru>
2023-04-02 21:26:39 +0500

    something which in comment is make an connection error

--- src/main/java/ru/netology/Homework35/httpServer/Main.java
+++ src/main/java/ru/netology/Homework35/httpServer/Main.java
@@ -9,25 +9,12 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
-//        final var validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html",
-//                "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
-//
-//        Server server = new Server(validPaths);
-//        server.start();
 public class Main {
     public static final int THREADS_NUMBER = 64;
 
     public static void main(String[] args) {
         final var server = new Server();
-
-
         server.addHandler("GET", "/messages", new Handler() {
             public void handle(Request request, BufferedOutputStream out) throws IOException {
                 final var filePath = Path.of(".", "public", "/index.html");
@@ -45,11 +32,7 @@ public class Main {
 
         server.addHandler("POST", "/messages", new Handler() {
             public void handle(Request request, BufferedOutputStream out) throws IOException {
-                // TODO: handlers code
                 String time = request.getBody();
-//                System.out.println((LocalDateTime.now()).format(DateTimeFormatter.ofPattern("d.MM.uuuu  HH:mm:ss")));
-
-
                 final var filePath = Path.of(".", "public", "/classic.html");
                 final var template = Files.readString(filePath);
                 final var content = template.replace("{time}", time);
@@ -60,7 +43,7 @@ public class Main {
                             "Connection: close\r\n" +
                             "\r\n").getBytes());
                 out.write(content.getBytes());
-                    out.flush();
+                out.flush();
             }
         });
         server.listen(9999);
--- src/main/java/ru/netology/Homework35/httpServer/server/Server.java
+++ src/main/java/ru/netology/Homework35/httpServer/server/Server.java
@@ -7,12 +7,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
@@ -22,21 +16,14 @@ import java.util.stream.IntStream;
 import static ru.netology.Homework35.httpServer.Main.THREADS_NUMBER;
 
 public class Server {
-//    private ConcurrentMap<String, String> requestMethodType = new ConcurrentHashMap<>();                    //d
-//    private List<String> validPaths = new ArrayList<>();                                                       //d
-//    private ConcurrentMap<String, Object> validPaths = new ConcurrentHashMap<>();                          //d
-    private ConcurrentMap<String, Handler> getHandlers = new ConcurrentHashMap<>();                          //1
-    private ConcurrentMap<String, Handler> postHandlers = new ConcurrentHashMap<>();                          //1
-
+    private final ConcurrentMap<String, Handler> getHandlers = new ConcurrentHashMap<>();                          //1
+    private final ConcurrentMap<String, Handler> postHandlers = new ConcurrentHashMap<>();
     private int port = 8080;
     final ExecutorService threadPool = Executors.newFixedThreadPool(64);
 
-    public Server() {
-    }
 
 
-
-    public boolean addHandler(String requestMethod, String path, Handler handler) {                    //0
+    public boolean addHandler(String requestMethod, String path, Handler handler) {
         if (requestMethod.equalsIgnoreCase("get")) {
             getHandlers.put(path, handler);
             return true;
@@ -60,21 +47,6 @@ public class Server {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     public void start() {
         try (final var serverSocket = new ServerSocket(port)) {
             System.out.println("The server has been started");
@@ -82,7 +54,7 @@ public class Server {
             IntStream.range(0, THREADS_NUMBER)
                     .forEach(i -> {
                         Runnable task = () ->
-                                this.handle(serverSocket); //переделать без Request request, BufferedOutputStream bufferedOutputStream
+                                this.handle(serverSocket);
                         threadPool.execute(task);
                         System.out.println("thread " + i + " run");
                     });
@@ -95,7 +67,6 @@ public class Server {
         }
     }
 
-    //1 переделать без Request request, BufferedOutputStream bufferedOutputStream
     public void handle(ServerSocket serverSocket) {
         while (true) {
             try (final var socket = serverSocket.accept();
@@ -104,34 +75,27 @@ public class Server {
                 final var requestLine = in.readLine();
                 final var parts = requestLine.split(" ");
                 if (parts.length != 3) {
+                    System.out.println("A wrong request");
                     return;
                 }
-
                 final var requestMethod = parts[0];
                 final var path = parts[1];
-                String requestHeaders = "Accept: " + "text/html" + "\r\n" +
-                        "Accept-encoding: " + "gzip" + "\r\n" +
-                        "Accept-language: " + "en-US,en;q=0.9" + "\r\n" +
-                        "Connection: keep-alive\r\n" +
-                        "\r\n";
-                StringBuilder stringBuilder = new StringBuilder();
-                String body;
-                String line;
-                while ((line=in.readLine()) != null) {
-                    stringBuilder.append(line);
-                    stringBuilder.append("\n");
-                }
-                body = stringBuilder.toString();
-                System.out.println(body);
 
-
-
-
-
-                final var request = new Request(requestMethod, requestHeaders, in.toString(), path);   //body!!!!!!!
-
-
-
+                String body = null;
+                String requestHeaders = null;
+//                String line;
+//                final var stringBuilder = new StringBuilder();
+//                while ((line=in.readLine()) != null) {
+//                    stringBuilder.append(line);
+//                    stringBuilder.append("\n");
+//                }
+//                var requestToParse = stringBuilder.toString();
+//                int bodyStart = requestToParse.indexOf("\n\n");
+//                String requestHeaders = requestToParse.substring(0, bodyStart);
+//                if (requestMethod.equalsIgnoreCase("post")) {
+//                    body = requestToParse.substring(bodyStart+2, requestToParse.length());
+//                }
+                final var request = new Request(requestMethod, requestHeaders, body, path);
                 var handler = findHandler(requestMethod, path);
                 if (handler == null) {
                     out.write((
