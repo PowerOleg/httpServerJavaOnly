eea0815b7fbeb9c2eb81c9d5f9d367c0880fd18f
PowerOleg <geraskin.oleg@mail.ru>
2023-04-15 13:42:33 +0500

    PostController redesigned for spring

---
+++ src/main/java/ru/netology/Homework39/dependencyInjection/configurations/WebConfig.java
@@ -0,0 +1,14 @@
+package ru.netology.Homework39.dependencyInjection.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+
+@Configuration
+public class WebConfig {
+    public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
+        final var bean = new RequestMappingHandlerAdapter();
+        bean.getMessageConverters().add(new GsonHttpMessageConverter());
+        return bean;
+    }
+}
--- src/main/java/ru/netology/Homework39/dependencyInjection/controllers/PostController.java
+++ src/main/java/ru/netology/Homework39/dependencyInjection/controllers/PostController.java
@@ -1,47 +1,39 @@
 package ru.netology.Homework39.dependencyInjection.controllers;
 
 import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.Homework39.dependencyInjection.models.Post;
 import ru.netology.Homework39.dependencyInjection.services.PostService;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Reader;
+import java.util.List;
 
-
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-    public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
-    private final Gson gson;
-
 
     public PostController(PostService service) {
         this.service = service;
-        gson = new Gson();
     }
 
-    public void all(HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var data = service.all();
-        response.getWriter().print(gson.toJson(data));
+    @GetMapping
+    public List<Post> all() throws IOException {
+        return service.all();
     }
 
-    public void getById(long id, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var post = service.getById(id);
-        response.getWriter().println(gson.toJson(post));
+    @GetMapping("/{id:[\\d]+}")
+    public Post getById(@PathVariable long id) throws IOException {
+        return service.getById(id);
     }
 
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        response.setContentType(APPLICATION_JSON);
-        final var post = gson.fromJson(body, Post.class);
-        final var data = service.save(post);
-        response.getWriter().print(gson.toJson(data));
+    @PostMapping
+    public Post save(@RequestBody Post post) throws IOException {
+        return service.save(post);
     }
 
-    public void removeById(long id, HttpServletResponse response) {
-        response.setContentType(APPLICATION_JSON);
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id) {
         service.removeById(id);
-        response.setStatus(HttpServletResponse.SC_OK);
     }
 }
